440cde0f66d43aa8242cc85f2f4b24ee4af4e065
TeamMugiwara <teammugiwara28@gmail.com>
2020-12-10 16:31:45 +0600

    Insert Data to Expense Retrieve Data from Income

---
+++ app/src/main/java/edu/ucucite/androidbudget/Savings.java
@@ -0,0 +1,32 @@
+package edu.ucucite.androidbudget;
+
+import androidx.appcompat.app.AlertDialog;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import java.text.DateFormat;
+import java.util.Date;
+
+import edu.ucucite.androidbudget.Model.Data;
+
+public class Savings extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_savings);
+
+
+
+
+    }
+
+
+}
